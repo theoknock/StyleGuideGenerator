28edc3b8b224450917c659709e4eb248cd3c0b05
James Alan Bush <jamesalanbush1972@gmail.com>
2024-05-02 10:15:59 -0400

    Initial Commit

---
+++ StyleGuideGenerator/StyleGuideGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  StyleGuideGeneratorApp.swift
+//  StyleGuideGenerator
+//
+//  Created by Xcode Developer on 5/2/24.
+//
+
+import SwiftUI
+
+@main
+struct StyleGuideGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
